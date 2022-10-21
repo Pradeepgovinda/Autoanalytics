f3c13c0a34fbdfe34d48c6ee18b4fb728194b939
Pradeepgovinda <pgovindaraj710@gmail.com>
2022-10-21 21:52:56 +0530

    home page

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import * as React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+import 'font-awesome/css/font-awesome.min.css';
+import 'mdbootstrap/css/mdb.min.css';
+
+
+
 import App from "./App";
 
 
--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -5,51 +5,61 @@ import Records from './records.json';
 
 
 const Layout = () => {
-  const [stock, setStock] = useState(Records);
-    // let btnRef = useRef();
-    // const increaseQuantity = index => {
-    //     const currentItems = [...stock];
+  const [stock] = useState(Records);
+  
   return (
     <>
-      <nav id="sidebarMenu" className="collapse d-lg-block sidebar collapse">
+      <nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse">
         <div className="position-sticky">
-          <div className="list-group list-group-flush mx-3 mt-4">
-            <a className="navbar-brand" href="#">
-              <img src="./images/SmartcarLogo.png" alt="" />
+          <div className="group list-group-flush mx-3 mt-4">
+            <a className="navbar-brand" href="carlogo">
+              <img src="./images/smartcarLogo.png" alt="Smartcar" />
             </a>
             <li className="list-group-item py-1">
               <a className="nav-link" aria-current="page" href="/">Home</a>
             </li>
+            <li>
             <a
-              className="list-group-item list-group-item-action py-2 ripple"
+              className="list-group-item py-2"
               aria-current="true"
               data-mdb-toggle="collapse"
-              href="#collapseExample1"
+              href="filter-vehicles"
               aria-expanded="true"
-              aria-controls="collapseExample1"
+              aria-controls="collapsevehicle"
             >
               <span>Filter Vehicles By Age</span>
             </a>
+            </li>
+            
 
-            <ul id="collapseExample1" className="collapse show list-group list-group-flush">
+            <ul id="vehiclesList" className="collapse show group list-group-flush">
               <li className="list-group-item py-1">
-                <a href="" className="text-reset">21 Day Vehicles</a>
+                <a href="21-day-vehicles" className="text-reset">21 Day Vehicles</a>
               </li>
               <li className="list-group-item py-1">
-                <a href="" className="text-reset">40 Day Vehicles</a>
+                <a href="40-day-vehicles" className="text-reset">40 Day Vehicles</a>
               </li>
               <li className="list-group-item py-1">
-                <a href="" className="text-reset">60 Day Vehicles</a>
+                <a href="60-day-vehicles" className="text-reset">60 Day Vehicles</a>
               </li>
               <li className="list-group-item py-1">
-                <a href="" className="text-reset">100+ Day Vehicles</a>
+                <a href="100+_day-vehicles" className="text-reset">100+ Day Vehicles</a>
               </li>
             </ul>
+              <li className="list-group-item py-1">
+                <a href="prediction-model" className="nav-link">Go to Prediction Model Overview</a>
+              </li>
+              <li className="list-group-item py-1">
+                <a href="today-vehicles" className="nav-link">Today’s Vehicles Needing Action</a>
+              </li>
+              <li className="list-group-item py-1">
+                <a href="scanner" className="nav-link">VIN Scanner</a>
+              </li>
           </div>
         </div>
       </nav>
 
-      <nav id="main-navbar" className="navbar navbar-expand-lg navbar-light fixed-top">
+      <nav id="mainNavbar" className="navbar navbar-expand-lg ">
 
         <div className="container-fluid">
 
@@ -62,7 +72,7 @@ const Layout = () => {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <i className="fas fa-bars"></i>
+            <i className="fa fa-bars"></i>
           </button>
 
         </div>
@@ -70,65 +80,61 @@ const Layout = () => {
 
 
       <main className="sticky">
-         <div className="container solid"> {/*pt-4 */}
-          <div className="row Vehicles">
+        <div className="container vehicles"> {/*pt-4 */}
+          <div className="row online-vehicle">
             <div className="col-md-4">
               <div className="row">
                 <div className="col">
-               <h1>158</h1>
-                <p>Vehicles Online</p>
+                  <h1 className="vehicle-one">158</h1>
+                  <p>Vehicles Online <span className="percent">+12% </span><span className="mom">MOM </span> </p>
                 </div>
                 <div className="col">
-                <h1>32.91</h1>
-                <p>Average Online Age</p>
+                  <h1 className="vehicle-one">32.91</h1>
+                  <p>Average Online Age <span className="percent">-10%</span><span className="mom">MOM </span> </p>
                 </div>
               </div>
             </div>
             <div className="col-md-4">
-            <div className="row average">
+              <div className="row vehicle-inventory">
                 <div className="col">
-                <h1>210</h1>
-                <p>Vehicles In Inventory</p>
+                  <h1 className="vehicle-one">210</h1>
+                  <p>Vehicles In Inventory</p>
                 </div>
                 <div className="col">
-                <h1>45.26</h1>
-                <p>Average Owned Age</p>
+                  <h1 className="vehicle-one">45.26</h1>
+                  <p>Average Owned Age</p>
                 </div>
               </div>
             </div>
-            <div className="col-md-4">Stock</div>
+            <div className="stock col-md-4">
+              <p className="stock-one">Stock</p>
+              <input type="text" placeholder="Search" />
+            </div>
           </div>
         </div>
-        <hr/>
+        <hr />
       </main>
 
 
-<main className="bg1">
-{/* <div className="container justify-content-center ">
-  <div className="row jeep mt-5 mb-5">
-    <div className="cost">
-    <p>1C4RJFAG6JC272412, 2018 Jeep Grand Cherokee, Liklihood to sell w/in 30 Days: 0.26, Price: $20997 Cost: $18682.6</p>
-    </div>
-  </div> */}
-
-{stock.map((item) => (
-    <div className="container item text-center mt-3" key={item.name}>
-        <div className="row jeep mt-5 mb-5">
-            <div className="row cost">
-                <div className="col-6  img">
-                <img src={item.icon} className="img-fluid" />
+      <main className="vehicle-details">
+        {stock.map((item) => (
+          <div className="container item text-center mt-3" key={item.name}>
+            <div className="container vehicle-list">
+              <div className="row">
+                <div className="col-4  vehicle-image">
+                  <img src={item.icon} title={item.title} alt= {item.title} className="img-fluid" />
                 </div>
-                <div className="col center">
-                <p><span className="h1">{item.name} </span>
-                  <span className="h2">{item.model} {item.code} </span> 
-                  <span className="h3">{item.sell} {item.price}  </span></p>
+                <div className="col items">
+                  <p><span className="item-name">{item.name} </span>
+                    <span className="item-model">{item.model} {item.code} </span>
+                    <span className="item-sell">{item.sell}  </span>
+                    <span className="item-price">{item.price}</span></p>
                 </div>
+              </div>
             </div>
-        </div>
 
-    </div>
-))}
-{/* </div> */}
+          </div>
+        ))}
       </main>
       <Outlet />
     </>
